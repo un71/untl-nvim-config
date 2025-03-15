9af63eafedc0a58abb03ead077b75273ba1d716d
Daulet Tulepbergenov <daulet6368@gmail.com>
2025-03-15 14:42:10 +0500

    feat: up config

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,5 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim",         enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "rcarriga/nvim-notify",                enabled = false }
 }
